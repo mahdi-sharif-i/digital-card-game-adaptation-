04f74226621604d57f7c236c96f1b31e0f8bf1a2
mahdi <mahdisharifi720@gmail.com>
2025-12-24 11:24:54 +0330

    Add sort value, remove extra .slnx file

--- Assets/scripts/card script/CardData.cs
+++ Assets/scripts/card script/CardData.cs
@@ -6,6 +6,7 @@ public class CardData : ScriptableObject
 {
     [field: SerializeField] public Sprite CardSprite { get; private set;}
     [field: SerializeField] public int value { get; private set;}
+    [field: SerializeField] public int sotrValue { get; private set;}
     [field: SerializeField] public CardSuit suit { get; private set;}
 
 }
--- Assets/scripts/card script/CardModel.cs
+++ Assets/scripts/card script/CardModel.cs
@@ -3,16 +3,19 @@ using suitName;
 public class Card
 {
     private readonly CardData cardData;
-    public Card(CardData CD)
+    public Sprite CardSprite { get => cardData.CardSprite;}
+    public int value { get; set;}
+    public int sotrValue { get; set;}
+    public CardSuit suit { get; set;}
+
+        public Card(CardData CD)
     {
         cardData = CD;
         suit = CD.suit;
         value = CD.value;
+        sotrValue = CD.sotrValue;
     }
-    
-    public Sprite CardSprite { get => cardData.CardSprite;}
-    public int value { get; set;}
-    public CardSuit suit { get; set;}
 
+    
 
 }
